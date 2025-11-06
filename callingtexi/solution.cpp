69f641a4654eb4bee05bfbbc851bf03b6b353967
Muju <anwn0414@naver.com>
2025-11-07 00:18:01 +0900

    초기화 작업, main 수정해야 함

---
+++ callingtexi/solution.cpp
@@ -0,0 +1,28 @@
+struct Result
+{
+    int mX, mY;
+    int mMoveDistance;
+    int mRideDistance;
+    Result()
+    {
+        mX = mY = mMoveDistance = mRideDistance = -1;
+    }
+};
+
+void init(int N, int M, int L, int mXs[], int mYs[])
+{
+
+}
+int pickup(int mSX, int mSY, int mEX, int mEY)
+{
+    return 0;
+}
+Result reset(int mNo)
+{
+    Result ret;
+    return ret;
+}
+void getBest(int mNos[])
+{
+
+}
